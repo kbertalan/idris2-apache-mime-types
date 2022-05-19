4013745bc7408e6a48b5aac016627a3b7a0f6617
André Videla <andre.videla@gmail.com>
2022-05-19 19:14:55 +0100

    check mime types weekly

--- generator/generate.sh
+++ generator/generate.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
 mkdir -p src/Data/Mime/Apache
-output="src/Data/Mime/Apache/Raw.idr"
+output=$1
+if [ -z $output ] ; then
+    output="src/Data/Mime/Apache/Raw.idr"
+fi
 curl https://svn.apache.org/repos/asf/httpd/httpd/trunk/docs/conf/mime.types > mime.types
 (cd generator; idris2 -x generate_source Generator.idr) > $output
 rm mime.types
